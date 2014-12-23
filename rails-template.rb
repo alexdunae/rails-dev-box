edb6673cf3c16819b8c6a56bc52f365dc94da422
Alex Dunae <alex@dunae.ca>
2014-12-23 13:04:38 -0800

    Simplify Rails template

--- rails-template.rb
+++ rails-template.rb
@@ -3,6 +3,7 @@ gem 'bcrypt'
 gem_group :development, :test do
   gem 'quiet_assets'
   gem 'awesome_print'
+  gem 'thin'
 end
 
 gem_group :test do
@@ -15,12 +16,6 @@ gem_group :production do
   gem 'rails_12factor'
 end
 
-environment <<-'EOC'
-  config.generators do |g|
-    g.test_framework :mini_test, spec: false, fixture: true
-  end
-EOC
-
 lib 'tasks/testing.rake', <<-'EOC'
 %w(decorators presenters reporters services values workers).each do |key|
   Rails::TestTask.new("test:#{key}" => "test:prepare") do |t|
@@ -31,7 +26,6 @@ end
 EOC
 
 run 'bundle install'
-generate 'minitest:install'
 
 run 'rm README.rdoc'
 run 'touch README.md'
